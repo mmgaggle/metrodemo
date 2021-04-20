50b09a2da0efc94ee540cad561e14c3b829b9381
Chris Blum <cblum@redhat.com>
2021-04-20 18:13:23 +0200

    Stuff!

--- compiler.py
+++ compiler.py
@@ -1,15 +1,56 @@
-import pyodbc 
+import pyodbc
+import os
+from random import randint
+from time import sleep
+
+print('I am alive!')
+
+sqlAddr = os.getenv('SERVER', 'maria.sysbenchgraph')
+db = os.getenv('DB', 'test')
+user = os.getenv('USER', 'sysbench')
+password = os.getenv('PASS', 'sysbench')
+myname = os.getenv('MYNAME', 'UNKNOWN')
 
 cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
 cnxn.setencoding(encoding='utf-8')
 
 conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=RON\SQLEXPRESS;'
-                      'Database=TestDB;'
+                      'Server=${sqlAddr};'
+                      'Database=${db};'
+                      'uid=${user};'
+                      'pwd=${password};'
                       'Trusted_Connection=yes;')
- 
+
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM TestDB.dbo.Person')
- 
-for row in cursor:
-    print(row)
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS metroDemo (
+    Id TEXT PRIMARY KEY,
+    Finished BOOL
+);
+
+''')
+
+print('Creating table if it does not exist yet')
+conn.commit()
+
+cursor.execute('''
+INSERT INTO metroDemo (Id, Finished)
+VALUES
+('${myname}', false),
+''')
+print('Adding my job to the list')
+conn.commit()
+
+print('I am tired, let me sleep a bit')
+# Sleep for 10-30 seconds
+sleep(randint(10, 30))
+
+cursor.execute('''
+UPDATE metroDemo
+SET
+    Finished = true
+WHERE
+    Id = '${myname}';
+''')
+print('I am done working')
+conn.commit()
